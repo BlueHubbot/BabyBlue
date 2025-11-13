66e93d3440bf438afb2ae31616fa4c484325d337
sarmadaf24 <sarmad.afzali@gmail.com>
2025-11-13 20:34:53 +0000

    Fix nginx rendering to avoid missing env in Python

--- scripts/03-nginx-render.sh
+++ scripts/03-nginx-render.sh
@@ -17,43 +17,34 @@ set +o allexport
 ERP_CONF="/etc/nginx/sites-available/erp-${SITE}.conf"
 ERP_LINK="/etc/nginx/sites-enabled/erp-${SITE}.conf"
 
-python3 - <<PY
-import os
-repo = os.environ["REPO_DIR"]
-site = os.environ["SITE"]
+# رندر vhost اصلی ERP
+python3 - "$REPO_DIR" "$SITE" "$ERP_CONF" <<'PY'
+import sys, os
+repo, site, out_path = sys.argv[1:]
 tpl_path = os.path.join(repo, "templates", "nginx", "erp.conf.j2")
-out_path = os.environ["ERP_CONF"]
-
 with open(tpl_path) as f:
     txt = f.read()
-
 txt = txt.replace("{{ site_domain }}", site).replace("{{ site_name }}", site)
-
 with open(out_path, "w") as f:
     f.write(txt)
 PY
 
 ln -sfn "$ERP_CONF" "$ERP_LINK"
 
+# رندر vhost داکس اگر تعریف شده باشد
 if [[ -n "${DOCS_SITE:-}" ]]; then
   DOCS_CONF="/etc/nginx/sites-available/docs-${DOCS_SITE}.conf"
   DOCS_LINK="/etc/nginx/sites-enabled/docs-${DOCS_SITE}.conf"
 
-  python3 - <<PY
-import os
-repo = os.environ["REPO_DIR"]
-docs_domain = os.environ["DOCS_SITE"]
-docs_root = os.environ["DOCS_ROOT"]
+  python3 - "$REPO_DIR" "$DOCS_SITE" "$DOCS_ROOT" "$DOCS_CONF" <<'PY'
+import sys, os
+repo, docs_domain, docs_root, out_path = sys.argv[1:]
 tpl_path = os.path.join(repo, "templates", "nginx", "docs.conf.j2")
-out_path = os.environ["DOCS_CONF"]
-
 with open(tpl_path) as f:
     txt = f.read()
-
 txt = (txt
        .replace("{{ docs_domain }}", docs_domain)
        .replace("{{ docs_root }}", docs_root))
-
 with open(out_path, "w") as f:
     f.write(txt)
 PY
@@ -61,6 +52,7 @@ PY
   ln -sfn "$DOCS_CONF" "$DOCS_LINK"
 fi
 
+# حذف default برای جلوگیری از conflict
 if [[ -e /etc/nginx/sites-enabled/default ]]; then
   rm -f /etc/nginx/sites-enabled/default
 fi
